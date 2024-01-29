2c4dc4733dede1fc3c3cdf0e00e3426c1b821e0d
benr-dev <54710382+benr-dev@users.noreply.github.com>
2024-01-29 00:26:44 +0000

    feature: use component-lib in app

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -1,10 +1,11 @@
 import "react";
+import { MyButton } from '@benr-dev/component-lib';
 
 export default function App() {
   return (
     <div>
       <h1>Welcome to my app</h1>
-      {/* <MyButton /> */}
+      <MyButton />
     </div>
   );
 }
